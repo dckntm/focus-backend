76e8c4227be202d65aaf794877fdf0b57b3a7114
Ilya Katun <hypnospinner@gmail.com>
2020-03-26 18:09:47 +0300

    issues/8 add infra di

--- src/Focus.Service.ReportScheduler/Infrastructure/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Focus.Service.ReportScheduler.Infrastructure
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/Focus.Service.ReportScheduler/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Convey;
+using Convey.Persistence.MongoDB;
+using Focus.Service.ReportScheduler.Application.Common.Interface;
+using Focus.Service.ReportScheduler.Infrastructure.Repository;
+using Focus.Service.ReportScheduler.Infrastructure.Repository.Documents;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Focus.Service.ReportScheduler.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IConveyBuilder AddInfrastructure(this IConveyBuilder builder)
+        {
+            builder.Services
+                .AddTransient<IReportScheduleRepository, ReportScheduleRepository>();
+
+            return builder
+                .AddMongo()
+                .AddMongoRepository<ReportScheduleDocument, Guid>("report-templates");
+        }
+    }
+}
