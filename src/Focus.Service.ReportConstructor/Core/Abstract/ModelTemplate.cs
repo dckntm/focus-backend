683d29812d9950d8e04cadb12c5dcfde099c4465
Ilya Katun <hypnospinner@gmail.com>
2020-04-02 23:01:19 +0300

    issues/20 add module template abstraction (ctor)

---
+++ src/Focus.Service.ReportConstructor/Core/Abstract/ModelTemplate.cs
@@ -0,0 +1,8 @@
+namespace Focus.Service.ReportConstructor.Core.Abstract
+{
+    public abstract class ModuleTemplate
+    {
+        public string Title { get; set; }
+        public int Order { get; set; }
+    }
+}
