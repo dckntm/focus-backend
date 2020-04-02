13c46023447a39085943630a967d0d6cf56a8fc0
Ilya Katun <hypnospinner@gmail.com>
2020-04-02 23:01:47 +0300

    issues/20 add input type enum for ctor

---
+++ src/Focus.Service.ReportConstructor/Core/Enums/InputType.cs
@@ -0,0 +1,17 @@
+namespace Focus.Service.ReportConstructor.Core.Enums
+{
+    public enum InputType
+    {
+        ShortText,
+        LongText,
+        Email,
+        PhoneNumber,
+        Label,
+        Integer,
+        Decimal,
+        Financial,
+        MultipleChoiceOptionList,
+        SingleOptionSelect,
+        Boolean
+    }
+}
