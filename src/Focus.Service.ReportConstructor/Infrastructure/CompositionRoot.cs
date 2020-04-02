68472a0b743d3dd920e85bb636a8af2c3d14a655
Ilya Katun <hypnospinner@gmail.com>
2020-04-02 23:12:01 +0300

    issues/20 add infra composition root

---
+++ src/Focus.Service.ReportConstructor/Infrastructure/CompositionRoot.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Focus.Infrastructure.Common.Interfaces;
+using Focus.Infrastructure.Common.Persistence;
+using Focus.Service.ReportConstructor.Application.Services;
+using Focus.Service.ReportConstructor.Infrastructure.Persistence;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Focus.Service.ReportConstructor.Infrastructure
+{
+    public static class CompositionRoot
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            return services
+                .AddScoped<IReportTemplateRepository, ReportTemplateRepository>();
+        }
+
+        public static void ConfigureServices(this IConfiguration configuration, IServiceCollection services)
+        {
+            var mongoConfig = configuration.GetMongoConfigurationFromSection("mongodb");
+
+            services.AddTransient<IMongoConfiguration>(_ => mongoConfig);
+        }
+    }
+}
