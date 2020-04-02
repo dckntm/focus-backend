505df3934d3f7e693259f81c6b33624349776b72
Ilya Katun <hypnospinner@gmail.com>
2020-04-02 23:07:42 +0300

    issues/20 add ctor app composition root

---
+++ src/Focus.Service.ReportConstructor/Application/CompositionRoot.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Focus.Service.ReportConstructor.Application
+{
+    public static class CompositionRoot
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            // TODO: replace with application layer assembly
+
+            return services
+                .AddMediatR(Assembly.GetExecutingAssembly());
+        }
+    }
+}
