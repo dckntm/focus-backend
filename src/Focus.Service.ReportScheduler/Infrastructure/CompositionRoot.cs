fae20647de984365a3d71ef3ed4d25e58c421382
Ilya Katun <hypnospinner@gmail.com>
2020-04-02 23:30:19 +0300

    issues/20 add composition root 4 infra of scheduler + delete old report schedule doc

---
+++ src/Focus.Service.ReportScheduler/Infrastructure/CompositionRoot.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Focus.Infrastructure.Common.Interfaces;
+using Focus.Infrastructure.Common.Persistence;
+using Focus.Service.ReportScheduler.Infrastructure.Persistence;
+using Focus.Service.ReportScheduler.Application.Services;
+
+namespace Focus.Service.ReportScheduler.Infrastructure
+{
+    public static class CompositionRoot
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            return services
+                .AddScoped<IReportScheduleRepository, ReportScheduleRepository>();
+        }
+
+        public static void ConfigureServices(this IConfiguration configuration, IServiceCollection services)
+        {
+            var mongoConfig = configuration.GetMongoConfigurationFromSection("mongodb");
+
+            services.AddTransient<IMongoConfiguration>(_ => mongoConfig);
+        }
+    }
+}
