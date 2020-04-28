546c73144013e92c9c0c957da28b0022249d092f
Ilya Katun <hypnospinner@gmail.com>
2020-04-28 23:35:28 +0300

    issues/29 add dtos for reports

---
+++ src/Focus.Service.ReportProcessor/Application/Dto/ReportInfoDto.cs
@@ -0,0 +1,10 @@
+namespace Focus.Service.ReportProcessor.Application.Dto
+{
+    public class ReportInfoDto
+    {
+        public string Id { get; set; }
+        public string AssignedOrganizationId { get; set; }
+        public string ReportStatus { get; set; }
+        public string Deadline { get; set; }
+    }
+}
