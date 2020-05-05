01bca548977cef4fe114d20f53a789faf6cc731b
Ilya Katun <hypnospinner@gmail.com>
2020-05-05 18:57:57 +0300

    issues/31 fix typename passing for RabbitMQ message exchange

---
+++ src/Focus.Service.ReportScheduler/Infrastructure/Services/DateTimeService.cs
@@ -0,0 +1,9 @@
+using Focus.Service.ReportScheduler.Application.Services;
+
+namespace Focus.Service.ReportScheduler.Infrastructure.Services
+{
+    public class DateTimeService : IDateTimeService
+    {
+        
+    }
+}
