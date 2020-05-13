fb2e67fb2ae85ab77ae1d2c7f0a0ed223333a00c
Ilya Katun <hypnospinner@gmail.com>
2020-05-13 21:19:43 +0300

    issues/36 upd jwt security for all services

--- src/Focus.Api.Common/AuthHandlers.fs
+++ src/Focus.Api.Common/AuthHandlers.fs
@@ -10,8 +10,14 @@ module AuthHandlers =
     let accessDenied: HttpFunc -> HttpContext -> HttpFuncResult =
         setStatusCode 401 >=> text "Security: this user can't access service"
 
+    let accessDeniedForService: HttpFunc -> HttpContext -> HttpFuncResult =
+        setStatusCode 401 >=> text "Security: this service can't access service"
+
     let authorize: HttpFunc -> HttpContext -> HttpFuncResult =
         requiresAuthentication (challenge JwtBearerDefaults.AuthenticationScheme)
 
     let mustBeAdmin: HttpFunc -> HttpContext -> HttpFuncResult =
-        authorize >=> authorizeUser (fun u -> u.HasClaim("role", "HOA")) accessDenied
+        authorize >=> authorizeUser (fun u -> u.HasClaim(ClaimTypes.Role, "HOA")) accessDenied
+
+    let mustBeService: HttpFunc -> HttpContext -> HttpFuncResult = 
+        authorize >=> authorizeUser (fun u -> u.HasClaim(ClaimTypes.Role, "service")) accessDeniedForService
