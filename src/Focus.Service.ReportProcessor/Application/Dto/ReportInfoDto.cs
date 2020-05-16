96ab3f5e67aec0e9e634eb167e7c97dc8313d0e7
Ilya Katun <hypnospinner@gmail.com>
2020-05-16 13:31:16 +0300

    issues/45 add get queried reports query

--- src/Focus.Service.ReportProcessor/Application/Dto/ReportInfoDto.cs
+++ src/Focus.Service.ReportProcessor/Application/Dto/ReportInfoDto.cs
@@ -1,3 +1,6 @@
+using Focus.Service.ReportProcessor.Entities;
+using Focus.Service.ReportProcessor.Enums;
+
 namespace Focus.Service.ReportProcessor.Application.Dto
 {
     public class ReportInfoDto
@@ -7,5 +10,24 @@ namespace Focus.Service.ReportProcessor.Application.Dto
         public string AssignedOrganizationId { get; set; }
         public string ReportStatus { get; set; }
         public string Deadline { get; set; }
+    }
+
+    public static class ReportInfoDtoExtensions
+    {
+        public static ReportInfoDto AsInfoDto(this Report report)
+            => new ReportInfoDto
+            {
+                Title = report.Title,
+                Id = report.Id,
+                AssignedOrganizationId = report.AssignedOrganizationId,
+                ReportStatus = report.Status switch
+                {
+                    ReportStatus.InProgress => "InProgress",
+                    ReportStatus.Overdue => "Overdue",
+                    ReportStatus.Passed => "Passed",
+                    _ => ""
+                },
+                Deadline = report.Deadline.ToString("dd.MM.yyyy")
+            };
     }
 }
